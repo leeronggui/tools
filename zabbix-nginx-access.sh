c9e167b2a16be74c373543dfa9a65cf4f04ff1df
leeronggui <395428260@qq.com>
2016-12-19 22:30:49 +0800

    Update zabbix-nginx-access.sh

--- zabbix-nginx-access.sh
+++ zabbix-nginx-access.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y perl
+#auther: leehonr
 #install logtail
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install logcheck
+yum install -y logcheck perl-Statistics-Descriptive perl-TimeDate perl
